aa8b4846b90ff4d0daffdab550ad5966eba7492e
Ashir <ashir.rao26@gmail.com>
2022-02-07 16:42:15 -0800

    Added handle change

--- frontend/components/FormFields/GenericNumberInput.js
+++ frontend/components/FormFields/GenericNumberInput.js
@@ -1,9 +1,12 @@
 export default function GenericNumberInput(props) {
+  const handleChange = (e) => {
+    //use this to get team #
+  };
   return (
     <input
       style={{ width: "150px" }}
       type="number"
-      onChange={this.handleChange}
+      onChange={handleChange}
     ></input>
   );
 }
