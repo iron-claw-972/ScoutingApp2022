3a1196480df74a1dc8cde853717af445c5837310
Ashir <ashir.rao26@gmail.com>
2022-02-13 13:01:29 -0800

    Create Blue Alliance.js

---
+++ frontend/pages/display/BlueAlliance.js
@@ -0,0 +1,14 @@
+export default function BlueAlliance(props) {
+  const json_data = {};
+  return (
+    <div>
+      <h3>
+        <table>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+        </table>
+      </h3>
+    </div>
+  );
+}
