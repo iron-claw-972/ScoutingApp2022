b32eebe27066eb5b33a0055a265bd51a4753c323
Ashir <ashir.rao26@gmail.com>
2022-02-16 16:56:18 -0800

    PDF display attempt

--- frontend/pages/display/PitDisplay.js
+++ frontend/pages/display/PitDisplay.js
@@ -3,11 +3,18 @@ import DriveSpecs from "../../components/DisplayTypes/DriveSpecs";
 import ShooterSpecs from "../../components/DisplayTypes/ShooterSpecs";
 import Sponsors from "../../components/DisplayTypes/Sponsors";
 import Textbox from "../../components/DisplayTypes/Textbox";
+import dynamic from "next/dynamic";
 
 export default function PitDisplay(props) {
   const autoHeading = "Auto Capabilities";
   const autoCapabilities =
     "Deposit preloaded ball, intake 1-2 more balls, and deposit them.";
+  const teamHist =
+    "Iron Claw is an FRC team that operates through the Robotics Class at Los Gatos High School. It was founded with a strong belief in the importance of student leadership.";
+
+  const PDFViewer = dynamic(() => import("./PDFViewer"), {
+    ssr: false,
+  });
   return (
     <div>
       <h3>Publicity Board</h3>
@@ -16,9 +23,9 @@ export default function PitDisplay(props) {
       <DriveSpecs></DriveSpecs>
       <Textbox category={autoHeading} text={autoCapabilities}></Textbox>
 
-      <iframe></iframe>
+      <PDFViewer />
 
-      <Sponsors></Sponsors>
+      <Textbox category={"About Us"} text={teamHist}></Textbox>
     </div>
   );
 }
---
+++ frontend/pages/scout/scan.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { QrReader } from "@blackbox-vision/react-qr-reader";
+import axios from "axios";
+
+export default function ScanQRCode() {
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (result) {
+            console.log(result?.text);
+          } else {
+            console.info(error);
+          }
+        }}
+        className="col-4"
+      />
+    </>
+  );
+}
