0ef1918c0eb1caf8928a447c55c15cc1cc8a9448
Ashir <ashir.rao26@gmail.com>
2022-02-19 11:41:24 -0800

    testing on monitor

--- frontend/pages/display/PDFViewer.js
+++ frontend/pages/display/PDFViewer.js
@@ -3,7 +3,7 @@ import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfViewer = () => (
-  <Document file={"./TechBinder.js"}>
+  <Document file={"../../public/TechBinder.pdf"}>
     <Page pageNumber={1} width={400} />
   </Document>
 );
