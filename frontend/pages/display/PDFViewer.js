daa876b90abdb404719f16ac565c33a7c2c8f9f4
Ashir <ashir.rao26@gmail.com>
2022-02-20 00:26:46 -0800

    the pdf works now

--- frontend/pages/display/PDFViewer.js
+++ frontend/pages/display/PDFViewer.js
@@ -1,11 +1,28 @@
-import { Document, Page } from "react-pdf";
-import { pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+// import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
 
-const PdfViewer = () => (
-  <Document file={"../../public/TechBinder.pdf"}>
-    <Page pageNumber={1} width={400} />
-  </Document>
-);
+const PdfViewer = () => {
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
+  pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+  return (
+    <Document file="../TechBinder.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      {Array.from({ length: numPages }, (_, index) => (
+        <Page
+          key={`page_${index + 1}`}
+          pageNumber={index + 1}
+          renderAnnotationLayer={false}
+          renderTextLayer={false}
+        />
+      ))}
+    </Document>
+  );
+};
 
 export default PdfViewer;
